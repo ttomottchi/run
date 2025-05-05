f09aa6519790b7050028e3783b3d1519d52e1996
ttomottchi <nigatsutouka@gmail.com>
2025-05-05 20:42:15 +0900

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -125,7 +125,7 @@ function movePlayer() {
 
 function spawnEnemies() {
   if (frameCount % ENEMY_INTERVAL === 0) {
-    let type = random(["front", "front", "left", "right"]);
+    let type = random(["front", "left", "right", "left", "right"]);
     let x = type === "front" ? random(width / 2 - 60, width / 2 + 60) :
             (type === "left" ? 0 : width);
     enemies.push({ x, y: -100, type, vx: 0, vy: ENEMY_SPEED_BASE, t: 0 });
